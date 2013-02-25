2bf9b54524d730bc7427ac72a3680cb5738f1d46
Nick Donaldson <ndonald2@gmail.com>
2013-02-24 21:05:08 -0500

    Control Adder implemented, need to test

---
+++ Tonic/Core/ControlAdder.cpp
@@ -0,0 +1,34 @@
+//
+//  ControlAdder.cpp
+//  Tonic
+//
+//  Created by Nick Donaldson on 2/24/13.
+//  Copyright (c) 2013 Morgan Packard. All rights reserved.
+//
+
+#include "ControlAdder.h"
+
+namespace Tonic { namespace Tonic_{
+  
+  bool ControlAdder_::computeHasChanged(const SynthesisContext_ &context){
+    for (unsigned int i=0; i<inputs.size(); i++){
+      if (inputs[i].hasChanged(context)){
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  TonicFloat ControlAdder_::computeValue(const SynthesisContext_ &context){
+    TonicFloat sum = 0.0f;
+    for (unsigned int i=0; i<inputs.size(); i++){
+      sum += inputs[i].getValue(context);
+    }
+    return sum;
+  }
+  
+} // Namespace Tonic_
+  
+  
+  
+} // Namespace Tonic
---
+++ Tonic/Core/ControlAdder.h
@@ -0,0 +1,124 @@
+//
+//  ControlAdder.h
+//  Tonic 
+//
+//  Created by Nick Donaldson on 2/24/13.
+//  Copyright (c) 2013 Morgan Packard. All rights reserved.
+//
+
+/*+++++++++++++++++++++ License ++++++++++++++++++++
+
+Use this code for whatever you want. There are NO 
+RESTRICTIONS WHATSOVER. Modify it, repackage it, 
+sell it, get rich from it, whatever. Go crazy. If 
+you want to make mehappy, contribute to this 
+project, but feel free to just use the code as a 
+starting point for whatever you like.
+
+Note that Tonic is heavily indebted to STK
+https://ccrma.stanford.edu/software/stk/
+
+++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+#ifndef __Tonic__ControlAdder__
+#define __Tonic__ControlAdder__
+
+#include <vector>
+#include "ControlGenerator.h"
+#include "ControlValue.h"
+
+using std::vector;
+
+namespace Tonic {
+  
+  namespace Tonic_ {
+    
+    class ControlAdder_ : public ControlGenerator_{
+      
+    public:
+      
+      void in( ControlGenerator input );
+      
+      ControlGenerator & getInput(unsigned int index) { return inputs[index]; };
+      unsigned int numInputs() { return inputs.size(); };
+      
+    protected:
+      
+      bool computeHasChanged( const SynthesisContext_ & context );
+      TonicFloat computeValue( const SynthesisContext_ & context );
+      
+      vector<ControlGenerator> inputs;
+      
+    };
+    
+  }
+  
+  class ControlAdder : public TemplatedControlGenerator<Tonic_::ControlAdder_>{
+    
+  public:
+    
+    void in( ControlGenerator input ){
+      gen()->in( input );
+    }
+    
+    ControlGenerator & operator[](unsigned int index){
+      return gen()->getInput(index);
+    }
+    
+    unsigned int numInputs(){
+      return gen()->numInputs();
+    }
+  };
+  
+  static ControlAdder operator+(ControlGenerator a, ControlGenerator b){
+    ControlAdder adder;
+    adder.in(a);
+    adder.in(b);
+    return adder;
+  }
+  
+  static ControlAdder operator+(ControlGenerator a, float b){
+    ControlAdder adder;
+    adder.in(a);
+    adder.in(ControlValue(b));
+    return adder;
+  }
+  
+  static ControlAdder operator+(float a, ControlGenerator b){
+    ControlAdder adder;
+    adder.in(ControlValue(a));
+    adder.in(ControlGenerator(b));
+    return adder;
+  }
+  
+  static ControlAdder operator+(ControlAdder a, ControlGenerator b){
+    a.in(b);
+    return a;
+  }
+  
+  static ControlAdder operator+(ControlGenerator a, ControlAdder b){
+    b.in(a);
+    return b;
+  }
+  
+  static ControlAdder operator+(ControlAdder a, float b){
+    a.in(ControlValue(b));
+    return a;
+  }
+  
+  static ControlAdder operator+(float a, ControlAdder b){
+    b.in(ControlValue(a));
+    return b;
+  }
+  
+  static ControlAdder operator+(ControlAdder a, ControlAdder b){
+    for (unsigned int i=0; i<b.numInputs(); i++){
+      a.in(b[i]);
+    }
+    return a;
+  }
+}
+
+#endif /* defined(__Tonic__ControlAdder__) */
+
+
