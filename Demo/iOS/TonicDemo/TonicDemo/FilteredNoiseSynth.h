32d3416b98c46e3602becb3f1be05d358af2876c
Morgan Packard <morgan@morganpackard.com>
2013-02-26 08:47:17 -0500

    Tweaks to FilteredNoiseSynth.

--- Demo/iOS/TonicDemo/TonicDemo/FilteredNoiseSynth.h
+++ Demo/iOS/TonicDemo/TonicDemo/FilteredNoiseSynth.h
@@ -13,6 +13,7 @@
 #include "Synth.h"
 #include "SineWave.h"
 #include "Filters.h"
+#include "RampedValue.h"
 
 using namespace Tonic;
 
@@ -20,15 +21,18 @@ class FilteredNoiseSynth : public Synth{
 public:
   FilteredNoiseSynth(){
     vector<float> midiNums;
+    midiNums.push_back(-24);
     midiNums.push_back(0);
     midiNums.push_back(3);
     midiNums.push_back(7);
     midiNums.push_back(10);
     Noise noise = Noise();
     
+    ControlValue cutoff0 = registerMessage("cutoff", 1);
+    
     for(int i = 0; i < midiNums.size(); i++){
       Tonic::Generator tremelo = (SineWave().freq( randomFloat(0.1, 0.3) ) + 1.5) * 0.3;
-      Tonic::Generator cutoff = registerMessage("cutoff", 1).ramped() * mtof(midiNums.at(i) + 80);
+      Tonic::Generator cutoff = RampedValue().target(cutoff0) * mtof(midiNums.at(i) + 80);
       BPF24 filter = BPF24().Q( registerMessage("Q", 5) ).cutoff( cutoff );
       outputGen = outputGen + (noise >> filter) * tremelo;
     }
