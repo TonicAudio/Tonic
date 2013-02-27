d562c0de177a8e132b267f3bbcfd5fab2cdc3421
Morgan Packard <morgan@morganpackard.com>
2013-02-26 20:01:11 -0500

    Basic LFNoise and demo working.

---
+++ Demo/iOS/TonicDemo/TonicDemo/LFNoiseTestSynth.h
@@ -0,0 +1,32 @@
+//
+//  LFNoiseTest.h
+//  TonicDemo
+//
+//  Created by Morgan Packard on 2/26/13.
+//  Copyright (c) 2013 Morgan Packard. All rights reserved.
+//
+
+#ifndef TonicDemo_LFNoiseTest_h
+#define TonicDemo_LFNoiseTest_h
+
+#include "Tonic.h"
+#include "Synth.h"
+#include "SineWave.h"
+#include "Filters.h"
+#include "LFNoise.h"
+
+using namespace Tonic;
+
+class LFNoiseTestSynth : public Synth{
+public:
+  LFNoiseTestSynth(){
+
+    outputGen = SineWave().freq(500 + 500 * LFNoise().setFreq(100)) * 0.3;
+    
+  }
+};
+
+registerSynth(LFNoiseTestSynth);
+
+
+#endif
---
+++ Tonic/Utils/LFNoise.cpp
@@ -0,0 +1,61 @@
+//
+//  LFNoise.cpp
+//  TonicDemo
+//
+//  Created by Morgan Packard on 1/29/13.
+//  Copyright (c) 2013 Morgan Packard. All rights reserved.
+//
+
+/*+++++++++++++++++++++ License ++++++++++++++++++++
+
+Use this code for whatever you want. There are NO 
+RESTRICTIONS WHATSOVER. Modify it, repackage it, 
+sell it, get rich from it, whatever. Go crazy. If 
+you want to make mehappy, contribute to this 
+project, but feel free to just use the code as a 
+starting point for whatever you like.
+
+Note that Tonic is heavily indebted to STK
+https://ccrma.stanford.edu/software/stk/
+
+++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+#include "LFNoise.h"
+#include <algorithm>
+#include "Util.h"
+
+namespace Tonic {
+namespace Tonic_{
+
+
+LFNoise_::LFNoise_() : mCounter(0){
+  mFreqFrames.resize(kSynthesisBlockSize);
+}
+
+void  LFNoise_::setFreq(ControlGenerator freq){
+  mFreq = freq;
+}
+
+void LFNoise_::computeSynthesisBlock( const SynthesisContext_ &context ){
+  int remain = synthesisBlock_.frames();
+  TonicFloat* out = &synthesisBlock_[0];
+  do{
+    if (mCounter<=0) {
+			mCounter = sampleRate() / std::max<float>(mFreq.getValue(context), .001f);
+			mCounter = std::max<float>(1, mCounter);
+			float nextlevel = randomFloat(-1, 1);
+			mSlope = (nextlevel - mLevel) / mCounter;
+		}
+    int nsmps = std::min(remain, mCounter);
+    remain -= nsmps;
+    mCounter -= nsmps;
+    for(int i = 0; i < nsmps; i++){
+      *(out++) = mLevel;
+      mLevel += mSlope;
+    }
+  }while (remain);
+}
+  
+}
+
+}
