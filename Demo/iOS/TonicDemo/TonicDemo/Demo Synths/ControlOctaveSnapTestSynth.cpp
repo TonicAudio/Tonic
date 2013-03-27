bbd53610c901f930bc864915521b3b52aced8e2b
Morgan Packard <morgan@morganpackard.com>
2013-03-26 22:07:51 -0400

    Bigger range for ControlSnapToScaleTestSynth, and interactivity.

--- Demo/iOS/TonicDemo/TonicDemo/Demo Synths/ControlOctaveSnapTestSynth.cpp
+++ Demo/iOS/TonicDemo/TonicDemo/Demo Synths/ControlOctaveSnapTestSynth.cpp
@@ -18,6 +18,7 @@
 #import "ADSR.h"
 #import "ControlStepper.h"
 #import "ControlMetro.h"
+#import "ControlRandom.h"
 
 using namespace Tonic;
 
@@ -27,59 +28,72 @@ class ControlSnapToScaleTestSynth : public Synth{
    
     vector<float> scale = {0,2,3,7,10};
     
-    ControlMetro metro = ControlMetro().bpm(350);
+    ControlMetro switchIt = ControlMetro().bpm(30);
     
-    ADSR env = ADSR(0.01, 1.4, 0, 0)
+    ControlMetro metro = ControlMetro().bpm(
+        ControlStepper()
+          .start(550)
+          .end(650)
+          .step(100)
+          .trigger(switchIt)
+      );
+    
+    ADSR env = ADSR(0.01, 0.5, 0, 0)
       .trigger(metro)
       .doesSustain(false)
-      .legato(true);
+      .legato(true)
+      .decay(
+        ControlStepper()
+        .start(0.1)
+        .end(10)
+        .step(10.9)
+        .trigger(ControlMetro().bpm(30))
+      );
     
-    
+    ControlGenerator stepperStart = addParameter("stepperStart") * 30 + 43;
     
     ControlStepper stepper1 = ControlStepper()
-      .start(43)
-      .end(90)
-      .step(
-        ControlStepper().start(4).end(11).step(1).trigger(metro)
+    .start( stepperStart )
+    .end(stepperStart + 50 *  addParameter("stepperSpread"))
+    .step(
+      ControlStepper()
+      .start(4)
+      .end(
+        ControlRandom()
+        .min(9)
+        .max(15)
+        .trigger(
+          ControlMetro().bpm(11)
+        )
       )
-      .trigger(metro);
+      .step(1)
+      .trigger(metro)
+    )
+    .trigger(metro);
    
     ControlSnapToScale scaleSnapper1 =  ControlSnapToScale()
       .setScale(scale)
       .in( stepper1 );
     
-    
-    
-    
-    ControlStepper stepper2 = ControlStepper()
-      .start(43)
-      .end(92)
-      .step(
-        ControlStepper().start(4).end(13).step(1).trigger(metro)
-      )
-      .trigger(metro);
-   
-    ControlSnapToScale scaleSnapper2 =  ControlSnapToScale()
-      .setScale(scale)
-      .in( stepper2 );
-    
-    
-  
     outputGen = (
     
       SineWave().freq(
-        ControlMidiToFreq().in(scaleSnapper1)
-      )
-  
-      +
+        ControlMidiToFreq().in(scaleSnapper1 + -12).ramped().length(
+          ControlRandom().min(0).max(0.03).trigger(switchIt)
+        )
+      ) 
       
+      +
+    
       SineWave().freq(
-        ControlMidiToFreq().in(scaleSnapper2 + 24)
+        ControlMidiToFreq().in(scaleSnapper1 + 12)
       ) * 0.1
       
     )
     
-    * 0.3 * env >> StereoDelay(0.1,0.11).feedback(0.3);
+    * 0.3 * env >> StereoDelay(0.3,0.33).feedback(0.3).mix(
+      ControlRandom().min(0).max(0.1).trigger(switchIt).ramped()
+    );
     
     
   }
