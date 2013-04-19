e20fa6763416cce046596063eadec2805b99b004
Morgan Packard <morgan@morganpackard.com>
2013-04-18 22:22:09 -0400

    Logorithmic sustain makes more expressive bass. High pass filter.

--- Demo/iOS/TonicDemo/TonicDemo/Demo Synths/ControlSwitcherTestSynth.cpp
+++ Demo/iOS/TonicDemo/TonicDemo/Demo Synths/ControlSwitcherTestSynth.cpp
@@ -19,6 +19,9 @@
 #include "ADSR.h"
 #include "ControlSwitcher.h"
 #include "ControlCounter.h"
+#include "ControlStepper.h"
+#include "ControlRandom.h"
+#include "Filters.h"
 
 using namespace Tonic;
 
@@ -27,24 +30,56 @@ public:
   ControlSwitcherTestSynth(){
   
     ControlMetro metro = ControlMetro().bpm(80 * 4);
+    ControlGenerator modeSwitch =  ControlMetro().bpm(4);
     
     const int numSteps = 11;
     ControlCounter step = ControlCounter()
       .trigger(metro)
-      .end(numSteps - 1);
+      .end(
+      ControlSwitcher()
+      .addInput(numSteps)
+      .addInput( ControlRandom().min(2).max(6).trigger(modeSwitch) )
+      .inputIndex(
+        ControlCounter()
+        .end(2)
+        .trigger(modeSwitch)
+        + -1)
+      + -1);
+    
+    
+    ControlGenerator clickVol = addParameter("clickVol");
+    ADSR clickEnv =  ADSR(0, 0.01, 0, 0).trigger(metro);
+    Generator clickIntensity = (1 + clickVol).ramped();
+    Generator click = SineWave().freq(300 + 1000 * clickEnv ) * clickEnv * 0.03 ;
     
     ControlSwitcher fmAmount = ControlSwitcher().inputIndex(step);
     ControlSwitcher sustain = ControlSwitcher().inputIndex(step);
+    ControlSwitcher decay = ControlSwitcher().inputIndex(step);
     
     for(int i = 0; i < numSteps; i++){
-      
-      fmAmount.addInput(randomFloat(1, 10) + randomFloat(-5, 5) * addParameter("x"));
-      sustain.addInput(randomFloat(0, 0.7) + randomFloat(-0.5, 0.3) * addParameter("y"));
+      fmAmount.addInput(randomFloat(1, 10));
+      sustain.addInput(randomFloat(0, 0.7));
+      decay.addInput(randomFloat(0.03, 0.1));
     }
     
     Generator freq = ControlValue(50).ramped();
-    outputGen = SineWave().freq(freq + freq * SineWave().freq(freq * 2) * fmAmount.ramped());
-    outputGen = outputGen * ADSR(0.001, 0.1,0,0).legato(true).sustain(sustain).trigger(metro);
+    Generator tremelo =  1 + ( SineWave().freq(15) *  ADSR(0, 0.5, 0,0).trigger(modeSwitch) );
+    Generator bassEnv = ADSR(0.001, 0.1 ,0,0).decay(decay).legato(true).sustain(sustain * sustain).trigger(metro);
+    Generator bass =
+      SineWave()
+      .freq(
+        freq
+        + freq
+          * SineWave()
+            .freq(freq * 2)
+          * (
+            fmAmount
+            * 0.5
+            + addParameter("addtlFM")
+            ).ramped()
+      ) >> HPF12().cutoff(addParameter("hpf") * 1000) ;
+    Generator bassWithAmp = bass * bassEnv * tremelo;
+    outputGen = bassWithAmp  + click;
   }
 };
 
