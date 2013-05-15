73304d79a0a2c8bea7e30263fadc5c73ac337a2b
Morgan Packard <morgan@morganpackard.com>
2013-05-14 22:17:27 -0400

    Simple step sequencer

---
+++ Demo/Demo Synths/SimpleStepSeqSynth.cpp
@@ -0,0 +1,51 @@
+//
+//  SimpleStepSeqSynth.h
+//  TonicDemo
+//
+//  Created by Morgan Packard on 5/14/13.
+//  Copyright (c) 2013 Morgan Packard. All rights reserved.
+//
+
+#ifndef TonicDemo_SimpleStepSeqSynth_h
+#define TonicDemo_SimpleStepSeqSynth_h
+
+
+#include "Tonic.h"
+
+using namespace Tonic;
+
+class SimpleStepSeqSynth : public Synth {
+  
+
+public:
+  SimpleStepSeqSynth(){
+  
+    const int NUM_STEPS = 8;
+  
+    ControlGenerator metro = ControlMetro().bpm(4 * addParameter("tempo", 100, 50, 300));
+    
+    ControlGenerator step = ControlStepper().end(NUM_STEPS).trigger(metro);
+    ControlGenerator transpose = addParameter("transpose", 0, -6, 6);
+    ControlSwitcher pitches = ControlSwitcher().inputIndex(step);
+    ControlSwitcher cutoffs = ControlSwitcher().inputIndex(step);
+    
+    for(int i = 0; i < NUM_STEPS; i++){
+      float initialValue =  randomFloat(10, 80);
+      pitches.addInput(addParameter("step" + to_string(i) + "Pitch", initialValue, 10, 80, false ));
+      cutoffs.addInput(addParameter("step" + to_string(i) + "Cutoff", 500, 30, 1500, false));
+    }
+  
+    ControlGenerator midiNote = transpose + (pitches >> ControlSnapToScale().setScale({0,2,3,5,7,10}));
+  
+    outputGen = RectWave().freq( midiNote >> ControlMidiToFreq() ) * ADSR(0.01, 0.1, 0,0).trigger(metro) * 0.1;
+    outputGen = (outputGen >> LPF24().cutoff(cutoffs).Q(0.1));
+   // outputGen = outputGen >> Reverb().wetLevel(0.1).dryLevel(1).decayTime(1);
+    
+  }
+  
+};
+
+registerSynth(SimpleStepSeqSynth);
+
+
+#endif
