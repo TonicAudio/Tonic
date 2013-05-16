c49acf5a1fbe32cf1c7458d2c160af93d1d9f051
Nick Donaldson <ndonald2@gmail.com>
2013-05-15 22:36:47 -0400

    Fix demo for new ControlParameter scheme

--- Demo/Demo Synths/SimpleStepSeqSynth.cpp
+++ Demo/Demo Synths/SimpleStepSeqSynth.cpp
@@ -22,17 +22,17 @@ public:
   
     const int NUM_STEPS = 8;
   
-    ControlGenerator metro = ControlMetro().bpm(4 * addParameter("tempo", 100, 50, 300));
+    ControlGenerator metro = ControlMetro().bpm(4 * addParameter("tempo",100).min(50).max(300));
     
     ControlGenerator step = ControlStepper().end(NUM_STEPS).trigger(metro);
-    ControlGenerator transpose = addParameter("transpose", 0, -6, 6);
+    ControlGenerator transpose = addParameter("transpose", 0).min(-6).max(6);
     ControlSwitcher pitches = ControlSwitcher().inputIndex(step);
     ControlSwitcher cutoffs = ControlSwitcher().inputIndex(step);
     
     for(int i = 0; i < NUM_STEPS; i++){
       float initialValue =  randomFloat(10, 80);
-      pitches.addInput(addParameter("step" + to_string(i) + "Pitch", initialValue, 10, 80, false ));
-      cutoffs.addInput(addParameter("step" + to_string(i) + "Cutoff", 500, 30, 1500, false));
+      pitches.addInput(addParameter("step" + to_string(i) + "Pitch", initialValue).min(10).max(80));
+      cutoffs.addInput(addParameter("step" + to_string(i) + "Cutoff", 500).min(30).max(1500));
     }
   
     ControlGenerator midiNote = transpose + (pitches >> ControlSnapToScale().setScale({0,2,3,5,7,10}));
