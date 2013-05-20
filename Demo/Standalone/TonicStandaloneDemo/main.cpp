e8aa3b27222724143bd68d6873c52bd90cd9ce62
Morgan Packard <morgan@morganpackard.com>
2013-05-19 22:19:48 -0400

    Reverting accidentally committed changes to main.cpp in standalone demo

--- Demo/Standalone/TonicStandaloneDemo/main.cpp
+++ Demo/Standalone/TonicStandaloneDemo/main.cpp
@@ -44,19 +44,37 @@ int main(int argc, const char * argv[])
     // --------- MAKE A SYNTH HERE -----------
     
     synth = new Synth();
-  
-    synth->setLimitOutput(false);
-  
-//    ControlGenerator ctrlGen1 = ControlValue(2);
-//    Generator gen1 = ctrlGen1 + FixedValue(3) * ctrlGen1;
-  
-    ControlGenerator val1 = ControlValue(2);
-  ControlGenerator val2 = ControlValue(2);
-  ControlGenerator final = val1 + val1 * val2;
-  Tonic_::SynthesisContext_ context;
-  final.tick(context);
-  
-  //  synth->setOutputGen(FixedValue());
+    
+    ControlMetro metro = ControlMetro().bpm(100);
+    ControlGenerator freq = ControlRandom().trigger(metro).min(0).max(1);
+    
+    Generator tone = RectWave().freq(
+                                     freq * 0.25 + 100
+                                     + 400
+                                     ) * SineWave().freq(50);
+    
+    ADSR env = ADSR()
+    .attack(0.01)
+    .decay( 0.4 )
+    .sustain(0)
+    .release(0)
+    .doesSustain(false)
+    .trigger(metro);
+    
+    StereoDelay delay = StereoDelay(3.0f,3.0f)
+    .delayTimeLeft( 0.5 + SineWave().freq(0.2) * 0.01)
+    .delayTimeRight(0.55 + SineWave().freq(0.23) * 0.01)
+    .feedback(0.3)
+    .dryLevel(0.8)
+    .wetLevel(0.2);
+    
+    Generator filterFreq = (SineWave().freq(0.01) + 1) * 200 + 225;
+    
+    LPF24 filter = LPF24().Q(2).cutoff( filterFreq );
+    
+    Generator output = (( tone * env ) >> filter >> delay) * 0.3;
+    
+    synth->setOutputGen(output);
     
     // ---------------------------------------
     
