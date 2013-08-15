bc8a430b0e315863b15676906232375e72f9c613
Morgan Packard <morgan@morganpackard.com>
2013-08-15 01:00:46 -0400

    Added ControlScaleDegree

---
+++ src/Tonic/ControlScaleDegree.cpp
@@ -0,0 +1,31 @@
+//
+//  ControlScaleDegree.cpp
+//  Tonic
+//
+//  Created by Morgan Packard on 8/14/13.
+//  Copyright (c) 2013 Nick Donaldson. All rights reserved.
+//
+
+#include "ControlScaleDegree.h"
+
+namespace Tonic { namespace Tonic_{
+  
+  ControlScaleDegree_::ControlScaleDegree_(){
+    
+  }
+  
+  ControlScaleDegree_::~ControlScaleDegree_(){
+    
+  }
+  
+  void ControlScaleDegree_::computeOutput(const SynthesisContext_ & context){
+  
+  }
+  
+  
+  
+} // Namespace Tonic_
+  
+  
+  
+} // Namespace Tonic
