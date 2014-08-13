ac37d19091b5a37731decf250aadd845e919ee98
Nick Donaldson <nick.donaldson@raizlabs.com>
2014-08-13 12:38:47 -0400

    Get rid of unnecessary null checks. #245

--- src/Tonic/TonicCore.h
+++ src/Tonic/TonicCore.h
@@ -416,7 +416,7 @@ namespace Tonic {
       
       void release(){
         if(pcount && --(*pcount) == 0){
-          if (obj) delete obj;
+          delete obj;
           delete pcount;
           
           obj = NULL;
